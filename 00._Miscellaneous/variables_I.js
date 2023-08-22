d1bd3ccee71d546ec746eedcae484dda7ae601a4
Veronica Frydkjær <frida-nanni@hotmail.com>
2023-08-22 10:21:45 +0200

    nu med git add -A

---
+++ 00._Miscellaneous/variables_I.js
@@ -0,0 +1,29 @@
+const scheduledBreakTime = "09:27"
+//regel: brug const så meget som muligt, lav den altid const fra start
+
+let consoleLogCounter = 0
+
+console.log("Let's take a break at : ", scheduledBreakTime)
+//den outputter "at   :   09:27" fordi komma bruges^ frem for + ved concatinering
+//dvs vi har defineret usynlige regler for mig selv, brug komma i conole log
+//vi bruger , og ikke + for at undgå type coersion
+
+consoleLogCounter ++
+
+//datatyper:
+//Number, String, Boolean, BigInteger, undefined, null, Object, Symbol
+//Object: Object, Array, Date
+//Funktion er en datakonstrukt ifølge anders
+
+//type coersion
+//2 + "2" = '22'  - type coersion
+
+//How to make strings
+
+console.log("This is the first ''' way")
+console.log('This is the second """ way')
+console.log(`This is the third ${1+2}rd ''' """ way ${scheduledBreakTime}
+
+.`) //den her er milisekunder langsommere end de andre, 
+//betyder noget her. Multiline string literal og template string, kan bruge ${}
+consoleLogCounter += 3
