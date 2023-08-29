6e9da34d015b5b7e615cf812be5a914479ee9a8c
Veronica Frydkjær <frida-nanni@hotmail.com>
2023-08-29 15:10:30 +0200

    opdateret mountain api

--- Server_Mountains/app.js
+++ Server_Mountains/app.js
@@ -3,11 +3,12 @@ const express = require("express")
 const app = express() 
 
 const mountains = []
-mountains.push({id: 1, name: "Mount Everest"})
-mountains.push({id: 2, name: "K2"})
+mountains.push({id: 1, name: "Mauna Kea volcano"})
+mountains.push({id: 2, name: "Roraima"})
 mountains.push({id: 3, name: "Kilimanjaro"})
 mountains.push({id: 4, name: "Hvannadalshnúkur"})
 mountains.push({id: 5, name: "Himmelbjerget"})
+mountains.push({id: 5, name: "Hoverla"})
 
 
 app.get("/mountains", (req, res) =>{
@@ -17,13 +18,26 @@ app.get("/mountains", (req, res) =>{
 
 app.get("/mountains/:id", (req, res) => {
     let id = parseInt(req.params.id)
-
+    /*
+    første uoptimerede løsning:
     for (const mountain of mountains){
         if (mountain.id === id){
             res.send({mountain})
         }
     }
     res.send({message: `No mountain with id ${id}`})
+
+    Loop ved brug af forEach:
+    mountains.forEach((mountain) => {
+        if (mountain.id === id) {
+            res.send({mountain})
+        }
+      });
+    giver anderledes json, hvor "mountain" er key og objektet er value
+    frem for bare kun at få objekt som i de to andre
+    */
+   
+    res.send(mountains.find(mountain => mountain.id === id))
 })
 
 
