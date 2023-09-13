2259b20515a16b83e8e6f0c4fd1c36b219182655
Veronica Frydkjær <frida-nanni@hotmail.com>
2023-09-13 21:28:52 +0200

    boilerplate done

--- 03a._Time_Date_Server/app.js
+++ 03a._Time_Date_Server/app.js
@@ -3,10 +3,30 @@ const app = express()
 
 app.get("/", (req, res) => {
     
-    res.send({ data: "some data" })
+    res.sendFile( __dirname + "/public/index.html" )
+})
+
+app.get("/data", (req, res) => {
+    
+    res.send({ data: "data"})
+})
+
+app.get("/secondPage", (req, res) => {
+    
+    res.sendFile( __dirname + "/public/secondPage.html" )
 })
 
 
+
+
+const PORT = 8080
+app.listen(PORT, (error) => {
+    if(error){
+        console.log("failed", error)
+    } else {
+        console.log("server is running on ", PORT)
+    }
+})
 
 module.exports = app  //referere til const app = express() i start. Sikkert konvention at
 //express kaldes app?
